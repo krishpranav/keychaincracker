a813a66d8d7da6671ad7b133408a6e1206f7bebb
krishpranav <krisna.pranav@gmail.com>
2021-05-10 15:49:01 +0530

    update main m

--- keychaincracker/main.m
+++ keychaincracker/main.m
@@ -6,8 +6,9 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+@import Cocoa;
 
-int main(int argc, const char * argv[]) {
+int main( int argc, const char * argv[] )
+{
     return NSApplicationMain(argc, argv);
 }
